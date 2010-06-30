ec4c6baf4d5d0af460e1d6cc7c77213f86f7618e
Jonhnny Weslley <jw@jonhnnyweslley.net>
2010-06-30 12:07:06 -0300

    automatic configuration for gedit tools

--- configure.sh
+++ configure.sh
@@ -8,6 +8,11 @@ for f in `find -type f -name ".*"` ; do
 done
 ln -s `pwd`/.vim ~/.vim
 
+# install gedit tools
+for f in `ls gedit-tools/` ; do
+  ln -s `pwd`/gedit-tools/$f ~/.gnome2/gedit/tools/$f
+done
+
 # install my scripts under bin directory
 for f in `ls bin/` ; do
   ln -s `pwd`/bin/$f /usr/local/bin/$f
