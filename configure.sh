a53592e81c0baff924cb4bf8c743b2578b9e8847
Jonhnny Weslley <jonhnny@credishop.com.br>
2010-09-29 15:07:20 -0300

    dotfiles are placed just in root

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 cd ~/.dotfiles
 
 # link my configuration files
-for f in `find -type f -name ".*"` ; do
+for f in `find -maxdepth 1 -type f -name ".*"` ; do
   ln -s `pwd`/$f ~/$f
 done
 ln -s `pwd`/.vim ~/.vim
