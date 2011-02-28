db20600e0f752f78480eb38924b1449c817104eb
Jonhnny Weslley <jw@jonhnnyweslley.net>
2011-02-28 11:36:33 -0300

    fixed less bahaviour

--- lib/shell.bash
+++ lib/shell.bash
@@ -4,8 +4,9 @@ shopt -s cdspell
 shopt -s extglob
 shopt -s checkwinsize
  
-export PAGER="less"
 export EDITOR="vi"
+export PAGER="less"
+export LESS="-R -M"
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
