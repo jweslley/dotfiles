1e8ee30f1d79aafccf56444c54d55e8dbbb71b30
Jonhnny Weslley <jw@jonhnnyweslley.net>
2011-08-18 15:02:53 -0300

    rails footnotes support for firefox and gvim

---
+++ bin/firefox-gvim.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+#  Configure Firefox
+#  -----------------
+#
+#  Open about:config in your Firefox location bar and:
+#
+#  * Right-Click > New > String "network.protocol-handler.app.gvim" with content firefox-gvim.rb
+#
+#  * Right-Click > New > Boolean "network.protocol-handler.external.gvim" with value true
+#
+#  Update: In Firefox 3.5 and above you have to do:
+#
+#  * Right-Click: New > Boolean "network.protocol-handler.expose.gvim" with value false.
+#
+
+file = ARGV.first.split('file://').last.split('&').first
+line = /\&line\=(\d+)/.match(ARGV.first)[1] rescue 0
+`gvim --remote-tab-silent "+#{line}" #{file}`
+`wmctrl -a "GVIM"`
