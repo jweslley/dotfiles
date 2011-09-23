0a4e370e382c059eaed7eeedeaa565320acc5ca0
Jonhnny Weslley <jw@jonhnnyweslley.net>
2011-09-23 11:58:49 -0300

    improved rvm prompt which detects Gemfile in file tree

--- lib/prompt.bash
+++ lib/prompt.bash
@@ -11,6 +11,19 @@ function __rvm_ruby_version {
   [ -f 'Gemfile' ] && echo "$(rvm-prompt) "
 }
 
+function __rvm_ruby_version_with_gemfile_detector {
+  local gemfile_path=""
+  local current_dir="$PWD"
+  while [[ -z "$gemfile_path" && $current_dir != '/' ]] ; do
+    if [ -f "$current_dir/Gemfile" ] ; then
+      gemfile_path="$current_dir"
+    else
+      current_dir=$(dirname $current_dir)
+    fi
+  done
+  [ -z "$gemfile_path" ] || echo "$(rvm-prompt) $bblue$(basename $gemfile_path) "
+}
+
 # the prompt
 PS1="$bred\$(__rvm_ruby_version)$byellow\$(__git_branch)$red\$(__git_dirty)$reset_color$ "
 
