1930dd52d67bcf324d09e94b12d76d07eea0a06e
Jonhnny Weslley <jw@jonhnnyweslley.net>
2011-11-08 12:10:47 -0300

    simple prompt

--- lib/prompt.bash
+++ lib/prompt.bash
@@ -25,7 +25,8 @@ function __rvm_ruby_version_with_gemfile_detector {
 }
 
 # the prompt
-PS1="$bred\$(__rvm_ruby_version)$byellow\$(__git_branch)$red\$(__git_dirty)$reset_color$ "
+#PS1="$bred\$(__rvm_ruby_version)$byellow\$(__git_branch)$red\$(__git_dirty)$reset_color$ "
+PS1="$ "
 
 # Change the window title of X terminals
-PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
+PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~} $(__git_branch) $(__rvm_ruby_version) \007"'
