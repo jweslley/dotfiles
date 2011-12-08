6c92c760b86bd1f65700d808f54352d9b5524fd2
Jonhnny Weslley <jw@jonhnnyweslley.net>
2011-12-08 15:30:10 -0300

    minor chnages

--- configure.sh
+++ configure.sh
@@ -7,13 +7,3 @@ for f in `find -maxdepth 1 -type f -name ".*"` ; do
   ln -s `pwd`/$f ~/$f
 done
 ln -s `pwd`/.vim ~/.vim
-
-
-# Backup files into Dropbox
-
-if [ -d "$HOME/Dropbox" ]; then
-  ln -s "$HOME/Dropbox/data/Empathy" "$HOME/.local/share/TpLogger/logs"
-else
-  echo "Install Dropbox client"
-  exit 1
-fi
