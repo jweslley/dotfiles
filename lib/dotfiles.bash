9074b83d38a7f75411dd03bedf186ca7be9455b0
Jonhnny Weslley <jw@jonhnnyweslley.net>
2011-12-28 15:59:01 -0300

    export DOTFILES env var

--- lib/dotfiles.bash
+++ lib/dotfiles.bash
@@ -8,6 +8,7 @@ DOTFILES_PATH="$DOTFILES/bin"
 DOTFILES_PATH="$(find $DOTFILES/apps/ -executable -type f -exec dirname {} \; |\
         grep -v -e "\.git" -e "test" | sort | uniq | tr '\n' ':')$DOTFILES_PATH"
 
+export DOTFILES
 export PATH=$PATH:$DOTFILES_PATH
 
 # TODO .filesrc
