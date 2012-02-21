ecd30003278e24d5e32c7220137dc6c2c3de6d2b
Jonhnny Weslley <jw@jonhnnyweslley.net>
2012-02-21 09:21:52 -0300

    ignore history

--- lib/history.bash
+++ lib/history.bash
@@ -4,7 +4,7 @@ shopt -s cmdhist
 shopt -s lithist
 shopt -s histappend histreedit histverify
 
-export HISTIGNORE="&:pwd:ls:sl:l:ll:la:lh"
+export HISTIGNORE="&:pwd:ls:sl:l:ll:la:lh:h:history"
 export HISTFILESIZE=10000       # the bash history should save 10K commands
 export HISTCONTROL=erasedups    # don't put duplicate lines in the history.
 export HISTTIMEFORMAT='%F %T '  # display timestamp
