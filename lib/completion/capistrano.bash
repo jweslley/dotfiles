1e19dc72660785a1e7403999efc5265804e82ce5
Jonhnny Weslley <jw@jonhnnyweslley.net>
2012-03-29 20:46:04 -0300

    adds bash completion support for capistrano

---
+++ lib/completion/capistrano.bash
@@ -0,0 +1,16 @@
+# Bash completion support for capistrano.
+
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+
+_capcomplete() {
+  if [ -f Capfile ]; then
+    recent=`ls -t .cap_tasks~ Capfile 2> /dev/null | head -n 1`
+    if [[ $recent != '.cap_tasks~' ]]; then
+      cap --tasks --tool | cut -d" " -f2 > .cap_tasks~
+    fi
+    COMPREPLY=($(compgen -W "`cat .cap_tasks~`" -- ${COMP_WORDS[COMP_CWORD]}))
+    return 0
+  fi
+}
+
+complete -o default -o nospace -F _capcomplete cap
