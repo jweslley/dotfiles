d0e3acd38daf259c23b25252b2bf97bec4a35e9b
Jonhnny Weslley <jw@jonhnnyweslley.net>
2012-04-28 09:56:55 -0300

    changes after precise pangolin

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 # link my configuration files
 find ~/.dotfiles/ -maxdepth 1 -name ".*"        \
   ! -path ~/.dotfiles/ ! -path ~/.dotfiles/.git \
-  -exec echo ln -sf {} --target-directory=$HOME \;
+  -exec ln -sf {} --target-directory=$HOME \;
 
 # Install terminator config
 mkdir -p $HOME/.config/terminator
@@ -11,3 +11,7 @@ cp ~/.dotfiles/etc/terminator/config $HOME/.config/terminator
 
 # Install rbenv
 curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+rbenv bootstrap-ubuntu-11-10
+rbenv install 1.9.3-p125
+rbenv global 1.9.3-p125
+rbenv bootstrap
