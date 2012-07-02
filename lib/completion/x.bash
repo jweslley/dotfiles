d875f23e92dcdcdd3819732386c4baeee590d66c
Jonhnny Weslley <jw@jonhnnyweslley.net>
2012-07-02 15:31:03 -0300

    rename docs to x

--- lib/completion/x.bash
+++ lib/completion/x.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-_docs_complete() {
+_x_complete() {
   local cur commands
 
   COMPREPLY=()
   cur=${COMP_WORDS[COMP_CWORD]}
 
-  commands=`docs`
+  commands=`x`
   cur=`echo $cur | sed 's/\\\\//g'`
 
   COMPREPLY=($(compgen -W "${commands}" ${cur} | sed 's/\\\\//g') )
 }
 
-complete -o default -o nospace -F _docs_complete docs
+complete -o default -o nospace -F _x_complete x
