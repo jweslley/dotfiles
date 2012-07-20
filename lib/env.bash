58276d5534d433007b6cde3f18e5fda565f619f4
Jonhnny Weslley <jw@jonhnnyweslley.net>
2012-07-20 17:32:13 -0300

    remove unsed env vars

--- lib/env.bash
+++ lib/env.bash
@@ -5,11 +5,10 @@ CDPATH=.:$HOME:$HOME/code:/data:/usr/local
 JAVA_HOME=/usr
 M2_HOME=/usr/local/maven
 SCALA_HOME=/usr/local/scala
-RUBY_GEMS=/var/lib/gems/1.8
 PYTHONPATH=/usr/local/lib/python:$PYTHONPATH
 export JAVA_HOME PYTHONPATH
 
-export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$SCALA_HOME/bin:$RUBY_GEMS/bin:/usr/local/mongodb/bin/
+export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$SCALA_HOME/bin
 
 export HOSTFILE=$HOME/.hosts
 
