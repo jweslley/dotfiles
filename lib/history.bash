6f76989279df5ab2cc49d32dc71bc70ab1c593bc
Jonhnny Weslley <jw@jonhnnyweslley.net>
2012-08-01 17:16:52 -0300

    configure both HISTFILESIZE and HISTSIZE env vars

--- lib/history.bash
+++ lib/history.bash
@@ -5,6 +5,7 @@ shopt -s lithist
 shopt -s histappend histreedit histverify
 
 export HISTIGNORE="&:pwd:ls:sl:l:ll:la:lh:h:history"
+export HISTSIZE=10000           # the bash history should save 10K commands
 export HISTFILESIZE=10000       # the bash history should save 10K commands
 export HISTCONTROL=erasedups    # don't put duplicate lines in the history.
 export HISTTIMEFORMAT='%F %T '  # display timestamp
