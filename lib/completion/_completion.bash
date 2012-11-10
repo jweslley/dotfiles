61252cb04d71f396878de7524601fcda7a564bcd
Jonhnny Weslley <jw@jonhnnyweslley.net>
2012-11-10 12:15:36 -0300

    complete aliases too

--- lib/completion/_completion.bash
+++ lib/completion/_completion.bash
@@ -4,3 +4,11 @@ if [ -f /etc/bash_completion ]; then
   . /etc/bash_completion
   bind "set completion-ignore-case on"
 fi
+
+# complete aliases
+# git
+complete -o default -o nospace -F _git g
+
+# bundle
+complete -o default -F _bundle b
+complete -o default -F _bundle_exec be
