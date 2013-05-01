96f4c44d7492da4e5f44567936705e7782b1142f
Jonhnny Weslley <jw@jonhnnyweslley.net>
2013-05-01 20:39:54 -0300

    updates

--- configure.sh
+++ configure.sh
@@ -23,3 +23,6 @@ crontab -e
 # Dropbox
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 ~/.dropbox-dist/dropboxd
+
+# Calibre
+sudo python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main(install_dir='/home/jweslley/tools/')"
