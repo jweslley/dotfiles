5aabf0e345435c6f430304eaea72dd7e628644d8
Jonhnny Weslley <jonhnny@credishop.com.br>
2014-02-20 11:02:04 -0300

    atualiza capistrano completion

--- lib/completion/capistrano.bash
+++ lib/completion/capistrano.bash
@@ -6,7 +6,8 @@ _capcomplete() {
   if [ -f Capfile ]; then
     recent=`ls -t .cap_tasks~ Capfile 2> /dev/null | head -n 1`
     if [[ $recent != '.cap_tasks~' ]]; then
-      cap --tasks --tool | cut -d" " -f2 > .cap_tasks~
+      cap -T | awk '{print $2}' > .cap_tasks~
+      find config/deploy -type f -exec basename {} \; | cut -d. -f1 >> .cap_tasks~
     fi
     COMPREPLY=($(compgen -W "`cat .cap_tasks~`" -- ${COMP_WORDS[COMP_CWORD]}))
     return 0
