d0601f94620a5c23889641981d1f94a3399f37d8
Jonhnny Weslley <jw@jonhnnyweslley.net>
2014-05-18 10:50:51 -0300

    go env

--- lib/go.bash
+++ lib/go.bash
@@ -4,12 +4,5 @@
 
 export GOROOT=$HOME/tools/go
 export GOPATH=$HOME/code/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=$HOME/tools/go_appengine:$GOPATH/bin:$GOROOT/bin:$PATH
 export CDPATH=$CDPATH:$HOME/code/go/src
-
-gotools() {
-  go get github.com/nsf/gocode
-  go get github.com/jstemmer/gotags
-  go get github.com/mitchellh/gox
-  go get code.google.com/p/rog-go/exp/cmd/godef
-}
