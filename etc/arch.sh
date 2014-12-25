6e83006341416f9d842b893b0b502ce7fa4bb49c
Jonhnny Weslley <jw@jonhnnyweslley.net>
2014-12-25 14:08:45 -0300

    yaourt

--- etc/arch.sh
+++ etc/arch.sh
@@ -40,3 +40,17 @@ pacman --noconfirm -S firefox chromium mirage scrot calibre icedtea-web flashplu
 useradd -m -g users -G wheel,storage,power -s /bin/bash jweslley
 passwd jweslley
 echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+
+# yaourt
+curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+tar zxvf package-query.tar.gz
+cd package-query
+makepkg -si
+cd ..
+curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+tar zxvf yaourt.tar.gz
+cd yaourt
+makepkg -si
+cd ..
+
+yaourt -S chromium-pepper-flash
