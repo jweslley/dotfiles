6d329e7f4c6a00d878b29294a56fcbee6cf1d125
Jonhnny Weslley <jw@jonhnnyweslley.net>
2014-12-25 13:43:04 -0300

    arch stuff

--- configure.sh
+++ configure.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+# remove default files
+find ~ -maxdepth 1 -type f -name ".*" -exec rm {} \;
+
 # link my configuration files
 find ~/.dotfiles/ -maxdepth 1 -name ".*"        \
   ! -path ~/.dotfiles/ ! -path ~/.dotfiles/.git \
   -exec ln -sf {} --target-directory=$HOME \;
 
 # create basic directories
-mkdir ~/{bin,code,docs,musics,videos,images,tools}
+mkdir ~/{bin,code,docs,musics,videos,images,tools,.config}
 
 # youtube-dl config
-mkdir -p ~/.config
 ln -s ~/.dotfiles/etc/youtube-dl.conf ~/.config/
+
+# vim plugins
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# awesome-config
+git clone https://github.com/jweslley/awesome-config ~/.config/awesome
+
+# rbenv
+curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
---
+++ etc/arch.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Xorg
+pacman --noconfirm -S xorg-server xorg-server-utils xorg-apps xorg-xinit xscreensaver
+
+pacman --noconfirm -S xf86-video-fbdev xf86-video-modesetting
+# pacman --noconfirm -S xf86-video-DRIVER (lspci | grep -e VGA)
+# pacman --noconfirm -S mesa
+# pacman --noconfirm -S xf86-video-vesa
+
+# notebook
+# pacman --noconfirm -S xf86-input-synaptics laptop-mode-tools
+
+# window manager
+pacman --noconfirm -S awesome vicious faenza-icon-theme
+# mkdir -p .config/awesome
+
+# toolbox
+pacman --noconfirm -S rxvt-unicode openssh git bash-completion ttf-ubuntu-font-family net-tools ctags xclip unrar unzip pwgen lesspipe ack youtube-dl dosfstools colordiff rtorrent sudo tmux htop vim
+# vim+python => https://gist.github.com/MicahElliott/3048622
+
+# media
+pacman --noconfirm -S vlc pulseaudio-alsa pulseaudio
+pacman --noconfirm -S gstreamer0.10-base-plugins gstreamer0.10-good-plugins gstreamer0.10-bad-plugins gstreamer0.10-ugly-plugins gstreamer0.10-ffmpeg
+
+# desktop
+pacman --noconfirm -S firefox chromium mirage scrot calibre icedtea-web flashplugin gtk-chtheme
+
+# virtualization host
+# pacman --noconfirm -S virtualbox virtualbox-host-dkms
+
+# virtualization guest
+# pacman --noconfirm -S virtualbox-guest-modules virtualbox-guest-utils
+# modprobe -a vboxguest vboxsf vboxvideo
+# echo vboxguest >> /etc/modules-load.d/virtualbox.conf
+# echo vboxsf >> /etc/modules-load.d/virtualbox.conf
+# echo vboxvideo >> /etc/modules-load.d/virtualbox.conf
+
+# Users
+useradd -m -g users -G wheel,storage,power -s /bin/bash jweslley
+passwd jweslley
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
