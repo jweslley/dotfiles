98223c1f11c24a8a8d23768b48d2ead007dcd3f3
Jonhnny Weslley <jonhnny@credishop.com.br>
2016-10-11 18:19:40 -0300

    more tools

---
+++ lib/docker.bash
@@ -0,0 +1,3 @@
+# Useful tools via docker
+
+alias gitbook='docker run --rm -v "$PWD":/gitbook -p 4000:4000 billryan/gitbook gitbook'
--- lib/go.bash
+++ lib/go.bash
@@ -6,3 +6,5 @@ export GOROOT=$HOME/tools/go
 export GOPATH=$HOME/code/go
 export PATH=$HOME/tools/go_appengine:$GOPATH/bin:$GOROOT/bin:$PATH
 export CDPATH=$CDPATH:$HOME/code/go/src:$HOME/code/go/src/github.com/jweslley/
+
+alias golist="go list -f '{{ join .Imports \"\n\" }}'"
