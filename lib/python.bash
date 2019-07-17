61c22b6ef1f6289c1e4d75f8e42ae43231fc7a63
Jonhnny Weslley <jw@jonhnnyweslley.net>
2019-07-17 00:55:06 -0300

    Revert "use miniconda instead of anaconda"
    This reverts commit 3189a8c4f6609c27bff39219df05e73b26bb8f99.

--- lib/python.bash
+++ lib/python.bash
@@ -2,16 +2,4 @@
 
 # python development
 
-# Miniconda3 4.5.12 installer
-__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/tools/miniconda/bin/conda' shell.bash hook 2> /dev/null)"
-if [ $? -eq 0 ]; then
-  \eval "$__conda_setup"
-else
-  if [ -f "$HOME/tools/miniconda/etc/profile.d/conda.sh" ]; then
-    . "$HOME/tools/miniconda/etc/profile.d/conda.sh"
-    CONDA_CHANGEPS1=false conda activate base
-  else
-    \export PATH="$HOME/tools/miniconda/bin:$PATH"
-  fi
-fi
-unset __conda_setup
+export PATH="$HOME/tools/anaconda/bin:$PATH"
