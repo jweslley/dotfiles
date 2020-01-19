d9715bb825435a531dafe1d583ad3d4bcce56252
Jonhnny Weslley <jw@jonhnnyweslley.net>
2020-01-19 11:05:05 -0300

    add dc alias for docker-compose

--- lib/docker.bash
+++ lib/docker.bash
@@ -1,3 +1,5 @@
-# Useful tools via docker
+# docker-compose
+alias dc='docker-compose'
 
+# Useful tools via docker
 alias gitbook='docker run --rm -v "$PWD":/gitbook -p 4000:4000 billryan/gitbook gitbook'
