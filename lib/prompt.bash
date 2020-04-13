6eb3b263ba3f44ba44d287b2305ca1304442d77f
Jonhnny Weslley <jw@jonhnnyweslley.net>
2020-04-13 14:17:20 -0300

    change default prompt

--- lib/prompt.bash
+++ lib/prompt.bash
@@ -14,7 +14,7 @@ PROMPT_CLOCK="\[\033[s\]\[\033[1;\$((COLUMNS-4))f\]\$(date +%H:%M)\[\033[u\]"
 STATUS_COLOR='$([[ $EXIT_CODE = 0 ]] && echo -ne "\[$color_off\]" || echo -ne "\[$bred\]")'
 
 # the prompt
-PS1="\[$green\]\W\[$yellow\]$GIT_PS1$STATUS_COLOR»\[$color_off\] "
+PS1="\[$green\]\W\[$yellow\]$GIT_PS1$STATUS_COLOR\$\[$color_off\] "
 
 # the window title of X terminals
 PROMPT_COMMAND='EXIT_CODE=$?;echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~} \007"'
