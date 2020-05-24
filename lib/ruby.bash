b739dfbc9848b583757a53574bf3cba19bf3b8ce
Jonhnny Weslley <jw@jonhnnyweslley.net>
2020-05-24 11:04:53 -0300

    update rails settings

--- lib/ruby.bash
+++ lib/ruby.bash
@@ -5,9 +5,6 @@
 alias b='docker-compose run --rm web bundle'
 alias be='docker-compose run --rm web bundle exec'
 alias rails='docker-compose run --rm web bundle exec rails'
-alias webi='docker-compose run --rm web bin/setup'   # web Install
-alias webu='docker-compose run --rm web bin/update'  # web Update
-alias webx='docker-compose run --rm web' # web eXecute
-alias webr='docker-compose run --rm web bundle exec rails restart'
-alias webc='docker-compose run --rm web bundle exec rails console'
+alias webs='docker-compose run --rm web bin/setup' # web Setup
+alias webx='docker-compose run --rm web'           # web eXecute
 
