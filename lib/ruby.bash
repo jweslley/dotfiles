3a9e87663773e64011ddacf33705bfa45c5e0d3e
Jonhnny Weslley <jw@jonhnnyweslley.net>
2020-06-27 10:42:42 -0300

    update ruby aliases

--- lib/ruby.bash
+++ lib/ruby.bash
@@ -4,6 +4,8 @@
 
 alias b='docker-compose run --rm web bundle'
 alias be='docker-compose run --rm web bundle exec'
+alias bundle='docker-compose run --rm web bundle'
+alias yarn='docker-compose run --rm web yarn'
 alias rails='docker-compose run --rm web bundle exec rails'
 alias webs='docker-compose run --rm web bin/setup' # web Setup
 alias webx='docker-compose run --rm web'           # web eXecute
