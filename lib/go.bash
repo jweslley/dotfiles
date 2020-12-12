0db060977ebe05c72cefdbf7836cc035bfbd3812
Jonhnny Weslley <jw@jonhnnyweslley.net>
2020-12-12 16:46:03 -0300

    remove GOROOT

--- lib/go.bash
+++ lib/go.bash
@@ -2,9 +2,8 @@
 
 # go development
 
-export GOROOT=$HOME/tools/go
 export GOPATH=$HOME/code/go
-export PATH=$HOME/tools/go_appengine:$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=$HOME/tools/go_appengine:$GOPATH/bin:$PATH
 export CDPATH=$CDPATH:$HOME/code/go/src:$HOME/code/go/src/github.com/jweslley/
 
 alias golist="go list -f '{{ join .Imports \"\n\" }}'"
