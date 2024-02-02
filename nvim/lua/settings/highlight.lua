1db4f75450385dcf15dd3077707f4edf87c6442d
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-02-02 14:08:00 -0300

    nvim: add highlight with treesitter

---
+++ nvim/lua/settings/highlight.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
