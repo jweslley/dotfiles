55ba65f1f49c6322c20a29a358333473db29cffd
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-02-02 15:03:20 -0300

    nvim: add file tree with neo-tree

---
+++ nvim/lua/settings/filetree.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      default_component_configs = {
+        indent = {
+          indent_size = 1,
+          padding = 0,
+          with_markers = false,
+        },
+      },
+      window = {
+        position = "left",
+        width = 32,
+        mappings = {
+          ["h"] = "toggle_node",
+          ["l"] = "open",
+          ["q"] = "close_window",
+          ["s"] = "open_split",
+          ["v"] = "open_vsplit",
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>q", ":Neotree reveal<CR>", {})
+  end,
+}
