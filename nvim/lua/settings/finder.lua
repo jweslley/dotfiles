a08b60e38bc97220ee1295785da281654b16e645
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-02-03 11:18:04 -0300

    nvim: apply stylua

--- nvim/lua/settings/finder.lua
+++ nvim/lua/settings/finder.lua
@@ -1,26 +1,52 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					path_display = { "truncate" },
+					mappings = {
+						i = {
+							["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+							["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+							["<C-q>"] = require("telescope.actions").send_selected_to_qflist
+								+ require("telescope.actions").open_qflist,
+						},
+					},
+					extensions = {
+						["ui-select"] = {
+							require("telescope.themes").get_dropdown({}),
+						},
+					},
+				},
+			})
 
-      vim.keymap.set("n", "<leader>o", "<cmd>Telescope find_files theme=ivy<CR>", { desc = "Lists files in current directory" })
-      vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers theme=ivy<CR>", { desc = "Lists open buffers" })
-      vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep theme=ivy<CR>", { desc = "Search for a string in current directory" })
-      vim.keymap.set("n", "<leader>l", "<cmd>Telescope current_buffer_tags theme=ivy<CR>", { desc = "Lists all of the tags for the current buffer" })
+			vim.keymap.set(
+				"n",
+				"<leader>o",
+				"<cmd>Telescope find_files<CR>",
+				{ desc = "Lists files in current directory" }
+			)
+			vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers<CR>", { desc = "Lists open buffers" })
+			vim.keymap.set(
+				"n",
+				"<leader>g",
+				"<cmd>Telescope live_grep<CR>",
+				{ desc = "Search for a string in current directory" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>l",
+				"<cmd>Telescope current_buffer_tags<CR>",
+				{ desc = "Lists all of the tags for the current buffer" }
+			)
 
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- 2 spaces over tabs
 vim.opt.tabstop = 2
@@ -24,43 +24,42 @@ vim.opt.sidescroll = 1
 vim.wo.number = true
 
 -- folding
-vim.opt.foldmethod = 'indent'
+vim.opt.foldmethod = "indent"
 vim.opt.foldnestmax = 5
 vim.opt.foldlevel = 5
 
 -- keymaps
 
-vim.keymap.set('n', ';', ':')
+vim.keymap.set("n", ";", ":")
 
 -- navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- folding
-vim.keymap.set('n', 'z', 'za')
-vim.keymap.set('v', 'z', 'za')
+vim.keymap.set("n", "z", "za")
+vim.keymap.set("v", "z", "za")
 
 -- make possible to navigate within lines of wrapped lines
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
 
 -- clears the search register
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { silent = true })
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { silent = true })
 
 -- quickly get out of insert mode without your fingers having to leave the home row
-vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set("i", "jj", "<Esc>")
 
 -- quit all, very useful in vimdiff
-vim.keymap.set('n', 'Q', ':qa!<CR>')
+vim.keymap.set("n", "Q", ":qa!<CR>")
 
 -- jump to tag
-vim.keymap.set('n', 'tt', '<C-]>')
+vim.keymap.set("n", "tt", "<C-]>")
 
 -- indent lines
-vim.keymap.set('n', '<', '<<')
-vim.keymap.set('n', '>', '>>')
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
-
+vim.keymap.set("n", "<", "<<")
+vim.keymap.set("n", ">", ">>")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
