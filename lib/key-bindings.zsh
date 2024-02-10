5d1da1e1da163be9360ffe9ea676770f799fb002
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-02-10 10:42:33 -0300

    update keybindings

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -1,9 +1,9 @@
-bindkey -s 'đ' 'rg '              # Alt + f
-bindkey -s '§' 'git show\n'       # Alt + S
-bindkey -s 'ß' 'git s\n'          # Alt + s
-bindkey -s 'ł' 'git l\n'          # Alt + l
-bindkey -s 'ð' 'git diff\n'       # Alt + d
-bindkey -s 'þ' 'git push\n'       # Alt + p
-bindkey -s '↓' 'git up\n'         # Alt + u
-bindkey -s 'ø' 'git checkout '    # Alt + o
-bindkey -s '₢' 'git commit -m \"' # Alt + c
+bindkey -s '^[f' 'rg '              # Alt + f
+bindkey -s '^[S' 'git show\n'       # Alt + S
+bindkey -s '^[s' 'git s\n'          # Alt + s
+bindkey -s '^[l' 'git l\n'          # Alt + l
+bindkey -s '^[d' 'git diff\n'       # Alt + d
+bindkey -s '^[p' 'git push\n'       # Alt + p
+bindkey -s '^[u' 'git up\n'         # Alt + u
+bindkey -s '^[o' 'git checkout '    # Alt + o
+bindkey -s '^[c' 'git commit -m \"' # Alt + c
