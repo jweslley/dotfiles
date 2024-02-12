32e7b5d288041ae110fb805ec4939f31e6117a66
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-02-12 15:23:05 -0300

    nvim: install tabnine

--- nvim/lua/settings/completions.lua
+++ nvim/lua/settings/completions.lua
@@ -87,4 +87,19 @@ return {
       })
     end,
   },
+  {
+    "codota/tabnine-nvim",
+    build = "./dl_binaries.sh",
+    config = function()
+      require("tabnine").setup({
+        disable_auto_comment = true,
+        accept_keymap = "<C-s>",
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt" },
+        log_file_path = nil, -- absolute path to Tabnine log file
+      })
+    end,
+  },
 }
