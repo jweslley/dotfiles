bd6a95a5dc0dfeb9dcb1381499a115b0f9ecebc9
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-02-12 15:22:47 -0300

    imprive zsh keybiding

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -1,5 +1,5 @@
 bindkey -s '^[f' 'rg '              # Alt + f
-bindkey -s '^[S' 'git show\n'       # Alt + S
+bindkey -s '^[S' 'git show '        # Alt + S
 bindkey -s '^[s' 'git s\n'          # Alt + s
 bindkey -s '^[l' 'git l\n'          # Alt + l
 bindkey -s '^[d' 'git diff\n'       # Alt + d
