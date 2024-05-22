293fbe2e633ca82b626dc311dc647202c8b5e18b
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-05-22 14:12:43 -0300

    add cspell

--- nvim/lua/settings/lsp.lua
+++ nvim/lua/settings/lsp.lua
@@ -141,10 +141,14 @@ return {
   },
   {
     "nvimtools/none-ls.nvim", -- configure formatters & linters
+    dependencies = {
+      "davidmh/cspell.nvim",
+    },
     lazy = false,
     config = function()
       local null_ls = require("null-ls")
       local null_ls_utils = require("null-ls.utils")
+      local cspell = require('cspell')
 
       local formatting = null_ls.builtins.formatting -- to setup formatters
       local diagnostics = null_ls.builtins.diagnostics -- to setup linters
@@ -156,6 +160,8 @@ return {
           formatting.prettier,
           formatting.rubocop,
           diagnostics.rubocop,
+          cspell.diagnostics,
+          cspell.code_actions,
         },
       })
     end,
