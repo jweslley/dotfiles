ed98d12612aefdded5c742c2c020d1627f000abc
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-05-22 15:36:41 -0300

    terminal improvements

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,11 +16,6 @@ return {
 
   enable_tab_bar = false,
 
-  quick_select_patterns = {
-    -- TODO: add pattern to match filename:filenumber
-    -- r"(?:[.\w\-@~]+)?(?:/[.\w\-@]+)+",
-  },
-
   disable_default_key_bindings = true,
   keys = {
     { key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
@@ -32,28 +27,5 @@ return {
     { key = "=", mods = "SUPER", action = act.IncreaseFontSize },
     { key = "-", mods = "SUPER", action = act.DecreaseFontSize },
     { key = "0", mods = "SUPER", action = act.ResetFontSize },
-
-    { key = "[", mods = "SUPER", action = act.ScrollByPage(-1) },
-    { key = "]", mods = "SUPER", action = act.ScrollByPage(1) },
-    { key = "k", mods = "SUPER", action = act.ClearScrollback("ScrollbackOnly") },
-
-    { key = "/", mods = "SUPER", action = act.Search("CurrentSelectionOrEmptyString") },
-    { key = "s", mods = "SUPER", action = act.QuickSelect },
-    { key = "x", mods = "SUPER", action = act.ActivateCopyMode },
-  },
-
-  key_tables = {
-    search_mode = {
-      { key = "Escape",   mods = "NONE", action = act.CopyMode("Close") },
-
-      { key = "j",        mods = "CTRL", action = act.CopyMode("NextMatch") },
-      { key = "k",        mods = "CTRL", action = act.CopyMode("PriorMatch") },
-
-      { key = "h",        mods = "CTRL", action = act.CopyMode("CycleMatchType") },
-      { key = "l",        mods = "CTRL", action = act.CopyMode("ClearPattern") },
-
-      { key = "PageUp",   mods = "NONE", action = act.CopyMode("PriorMatchPage") },
-      { key = "PageDown", mods = "NONE", action = act.CopyMode("NextMatchPage") },
-    },
   },
 }
--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,9 +1,11 @@
-bindkey -s '^[f' 'rg '              # Alt + f
-bindkey -s '^[S' 'git show '        # Alt + S
-bindkey -s '^[s' 'git s\n'          # Alt + s
-bindkey -s '^[l' 'git l\n'          # Alt + l
-bindkey -s '^[d' 'git diff\n'       # Alt + d
-bindkey -s '^[p' 'git push\n'       # Alt + p
-bindkey -s '^[u' 'git up\n'         # Alt + u
-bindkey -s '^[o' 'git checkout '    # Alt + o
-bindkey -s '^[c' 'git commit -m \"' # Alt + c
+bindkey -s '^[f' 'rg '                 # Alt + f
+bindkey -s '^[S' 'git show '           # Alt + S
+bindkey -s '^[s' 'git s\n'             # Alt + s
+bindkey -s '^[l' 'git l\n'             # Alt + l
+bindkey -s '^[d' 'git diff\n'          # Alt + d
+bindkey -s '^[r' 'git diff --cached\n' # Alt + r (stands for review)
+bindkey -s '^[p' 'git push\n'          # Alt + p
+bindkey -s '^[u' 'git up\n'            # Alt + u
+bindkey -s '^[o' 'git checkout '       # Alt + o
+bindkey -s '^[c' 'git commit -m \"'    # Alt + c
+bindkey -s '^['  'clear\n'             # Esc
