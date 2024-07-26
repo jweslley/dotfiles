e621c026a8de30b79891031837a918267e3d03a2
Jonhnny Weslley <jw@jonhnnyweslley.net>
2024-07-26 09:39:03 -0300

    update

--- nvim/lua/settings/testing.lua
+++ nvim/lua/settings/testing.lua
@@ -2,6 +2,7 @@ return {
   "nvim-neotest/neotest",
   event = "BufEnter *_spec.rb",
   dependencies = {
+    "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
